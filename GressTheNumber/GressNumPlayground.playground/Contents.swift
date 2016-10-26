e1f192de461540da04a55c4a9452479045aaa840
ChangAn1988 <alstonbrett@gmail.com>
2016-10-27 01:27:21 +0800

    猜範圍0至20中一個亂數答案的猜數字app 每場有6次的機會可以猜

---
+++ GressTheNumber/GressNumPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var randomNumber = arc4random()%21
+var randomNumber1 = arc4random()%21
+var randomNumber2 = arc4random()%21
+var randomNumber3 = arc4random()%21
+var randomNumber4 = arc4random()%21
+var randomNumber5 = arc4random()%21
+var randomNumber6 = arc4random()%21
+var randomNumber7 = arc4random()%21
+var randomNumber8 = arc4random()%21
+var randomNumber9 = arc4random()%21
+var randomNumber10 = arc4random()%21
+var randomNumber11 = arc4random()%21
+
+
+var gressNum:String = "abc"
+Int(gressNum)
+
+
--- GressTheNumber/ViewController.swift
+++ GressTheNumber/ViewController.swift
@@ -9,7 +9,100 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var guessNumber: UITextField!
+    
+    @IBOutlet weak var redPrompt: UILabel!
+    
+    @IBOutlet weak var bluePrompt: UILabel!
+    
+    var numOfTimes:Int = 7
+    
+    var ansNum = (arc4random() % 21)
+    
+    @IBAction func guessButton(_ sender: AnyObject) {
+        
+        print("答案是\(ansNum)")
+        
+        if Int(guessNumber.text!) == nil || guessNumber.text! == ""
+            || Int(guessNumber.text!)! < 0 || Int(guessNumber.text!)! > 20{
+            
+            redPrompt.text = "請輸入0~20範圍內的數字~!"
+            
+            redPrompt.isHidden = false
+            
+        } else if Int(guessNumber.text!)! >= 0 && Int(guessNumber.text!)! <= 20 {
+            
+            if numOfTimes >= 2 && numOfTimes <= 7 && Int(ansNum) == Int(guessNumber.text!)! {
+                
+                redPrompt.isHidden = false
+                
+                redPrompt.text = "你在第\(8 - numOfTimes)次時猜對了！"
+                
+                bluePrompt.text = "YOU WIN"
+                
+                numOfTimes = 8
+                
+                
+            } else {
+                
+                if numOfTimes >= 3 && numOfTimes <= 7 && Int(ansNum) > Int(guessNumber.text!)!{
+                    
+                    redPrompt.isHidden = false
+                    
+                    redPrompt.text = "猜錯了，太小了"
+                    
+                    numOfTimes -= 1
+                    
+                    bluePrompt.text = "還剩下\(numOfTimes - 1)次"
+                    
+                    //numOfTimes -= 1
+                    
+                } else if numOfTimes >= 3 && numOfTimes <= 7 && Int(ansNum) < Int(guessNumber.text!)! {
+                    
+                    redPrompt.isHidden = false
+                    
+                    redPrompt.text = "猜錯了，太大了"
+                    
+                    numOfTimes -= 1
+                    
+                    bluePrompt.text = "還剩下\(numOfTimes - 1)次"
+                    
+                    //numOfTimes -= 1
+                
+                } else if numOfTimes == 2{
+                    
+                    redPrompt.isHidden = false
+                    
+                    redPrompt.text = "次數用盡了，你輸了"
+                    
+                    bluePrompt.text = "YOU LOSE"
+                    
+                    numOfTimes = 8
+                    
+                } else if numOfTimes == 8 {
+                    
+                    redPrompt.text = "再玩一場？請按下方「重新玩」"
+                    
+                }
+            }
+        }
+    }
+    
+    @IBAction func rePlayBtn(_ sender: AnyObject) {
+        
+        redPrompt.isHidden = true
+        
+        bluePrompt.text = "可以猜6次"
+        
+        numOfTimes = 7
+        
+        ansNum = (arc4random() % 21)
+        
+        guessNumber.text = ""
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +113,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
